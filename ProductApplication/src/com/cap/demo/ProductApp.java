bd812c990bd2a146f7750b92c2d5e44acb42b8cc
Training <Training@DIN39000554.corp.capgemini.com>
2016-07-07 10:59:54 +0530

    commit

--- ProductApplication/src/com/cap/demo/ProductApp.java
+++ ProductApplication/src/com/cap/demo/ProductApp.java
@@ -5,7 +5,7 @@ public class ProductApp {
 	int[] a = {1,2,3,4};
 
 	public void productDisplay(){
-<<<<<<< HEAD
+
 		System.out.println("productDisplay - ProductApp");
 	}
 	
@@ -13,12 +13,5 @@ public class ProductApp {
 		System.out.println("Happy to see all the products");
 		System.out.println("Displaying all the products");
 		System.out.println("Display Added");
-=======
-		for (int i = 0; i < a.length; i++) {
-			System.out.println("Data:"+i);
-			System.out.println("Loop modified");
-			System.out.println("Loop modified 2");
-		}
->>>>>>> branch 'master' of https://github.com/TheAbhilash/DevOpsDay3.git
 	}
 }
